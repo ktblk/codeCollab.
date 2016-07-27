d5d976121666fe01cdd7835846802b30845ab422
Benjamin Feight <bjfeight@gmail.com>
2016-07-26 22:10:16 -0400

    almost authorized log-in

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get '/register' => 'users#new'
+  get '/login' => 'sessions#new'
+   post '/login' => 'sessions#create'
+   get '/logout' => 'sessions#destroy'
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
