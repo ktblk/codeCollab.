b56d0f3f4d335e14962d421e10da2a7dc63a9a1a
Benjamin Feight <bjfeight@gmail.com>
2016-07-28 09:04:01 -0400

    Text editor can be accessed at two locations

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,13 @@ Bundler.require(*Rails.groups)
 
 module Codecollab
   class Application < Rails::Application
+
+    config.before_initialize do
+        dev = File.join(Rails.root, 'config', 'config.yml')
+        YAML.load(File.open(dev)).each do |key,value|
+          ENV[key.to_s] = value
+      end
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
