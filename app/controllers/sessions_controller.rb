b393144c4e03a6a3c03456f4ca3e6fabe06f9544
ktblk <black.katherine.g@gmail.com>
2016-07-29 16:47:31 -0400

    key change

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,7 @@ class SessionsController < ApplicationController
       redirect_to root_path
     else
   # If user's login doesn't work, send them back to the login form.
-    redirect_to documents_path
+    redirect_to root_path
     end
   end
 
