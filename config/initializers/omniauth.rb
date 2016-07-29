d54dd2a7058e551cf90b64efc72350a746f5f98e
karen-lam <hikarenlam@gmail.com>
2016-07-29 18:27:50 -0400

    google auth changes 2.0

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,6 @@
 OmniAuth.config.logger = Rails.logger
+OmniAuth.config.on_failure = SessionsController.action(:new)
+
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, '966114572097-61o6d34pe4e9jvqqq9d60km2nk9k4ct4.apps.googleusercontent.com', '2cOyKWmgOhOd2m7WJPAkvnre'
