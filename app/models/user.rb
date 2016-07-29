12d9282e475a2fa157587c73c2ef40cc83876a31
karen-lam <hikarenlam@gmail.com>
2016-07-29 15:59:40 -0400

    google auth updates

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,9 +8,17 @@ class User < ApplicationRecord
       user.provider = auth.provider
       user.uid = auth.uid
       user.name = auth.info.name
+      # user.email = auth.info.email
       user.oauth_token = auth.credentials.token
       user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+      user.new_random_password
       user.save!
     end
+  end
+
+  # https://dzone.com/articles/generate-random-password-ruby
+  def new_random_password
+    self.password = Digest::SHA1.hexdigest("--#{Time.now.to_s}--#{name}--")[0,6]
+    self.password_confirmation = self.password
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   get 'auth/:provider/callback', to: 'sessions#create'
-   get 'auth/failure', to: redirect('/')
+  get 'auth/failure', to: redirect('/')
 
 
   get 'sessions/create'
