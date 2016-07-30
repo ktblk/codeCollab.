a7c83bf9584a4af6288414dbe68d0b7e1b8d0273
karen-lam <hikarenlam@gmail.com>
2016-07-30 11:37:49 -0400

    fixed merged conflict between master file and Google auth file

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
     resources :documents
   end
 
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'auth/failure', to: redirect('/')
+  
   get 'workspace' => 'workspaces#index'
 
     root to: 'documents#index'
