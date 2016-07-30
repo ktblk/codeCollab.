a8194f6a41d30c5981c592f63d1a866096d84087
ktblk <black.katherine.g@gmail.com>
2016-07-30 10:31:36 -0400

    works with multiple sessions and multiple users at one time. ..booyah.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,21 +1,2 @@
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
-//= require_tree .
-
-$(document).on( "click", '#workspace', function() {
-  var $this = $(this)
-      url = $this.data('url');
-
-  var ref = new Firebase('https://firepad.firebaseio-demo.com');
-
-  var hash = window.location.hash.replace(/#/g, '');
-
-  if (hash) {
-    ref = ref.child(hash);
-    console.log("HERE")
-  } else {
-    ref = ref.push(); // generate unique location.
-    window.location = window.location + url + '/#' + ref.key(); // add it as a hash to the URL.
-  }
-});
--- app/assets/javascripts/workspaces.js
+++ app/assets/javascripts/workspaces.js
@@ -1,24 +1,51 @@
-$(document).on('ready page:load', function () {
-
+$(function(){
   function init() {
     //// Initialize Firebase.
-    var firepadRef = new Firebase('https://firepad.firebaseio-demo.com');
+    var firepadRef = getRef();
 
     var editor = ace.edit("firepad-container");
     editor.setTheme("ace/theme/textmate");
     var session = editor.getSession();
     session.setUseWrapMode(true);
     session.setUseWorker(false);
-    session.setMode("ace/mode/ruby");
+    session.setMode("ace/mode/javascript");
+
     //// Create Firepad.
     var firepad = Firepad.fromACE(firepadRef, editor, {
       defaultText: '// JavaScript Editing with Firepad!\nfunction go() {\n  var message = "Hello, world.";\n  console.log(message);\n}'
     });
-
-    firepad.on('ready', function() {
-      console.log(firepad.getText())
-    });
   }
-  init();
 
+  function getRef() {
+    // var ref = new Firebase('https://firepad.firebaseio-demo.com');
+    var ref = new Firebase('https://firepadcollap.firebaseio.com/firepad');
+    var id = $('#currID').val();
+
+    var hash = window.location.hash.replace(/#/g, '');
+    if (hash) {
+      ref = ref.child(hash);
+    } else {
+      ref = ref.push(); // generate unique location.
+      window.location = window.location + '#' + ref.key() + "--" + id + "--"; // add it as a hash to the URL.
+    }
+    if (typeof console !== 'undefined')
+      console.log('Firebase data: ', ref.toString());
+    return ref;
+  }
+
+  window.onload = init;
+  var chatRef = new Firebase("https://firechat-demo.firebaseio.com");
+  var chat = new FirechatUI(chatRef, document.getElementById("firechat-wrapper"));
+  var username = $('#currName').val();
+  chatRef.onAuth(function(authData) {
+    if (authData) {
+      chat.setUser(authData.uid, username);
+    } else {
+      chatRef.authAnonymously(function(error, authData) {
+        if (error) {
+          console.log(error);
+        }
+      });
+    }
+  });
 })
